b3f236cf3ed06f232a9ba43aa21e89605539142e
Corey <corey260596@gmail.com>
2024-03-03 23:23:26 +1000

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState<string[]>([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex" }}>
+          <div style={{ margin: "30px" }}>
+            Main List (Today)
+            <ul>
+              {list.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div style={{ margin: "30px" }}>
+            Job Search
+            <ul>
+              <li
+                onClick={(e) => {
+                  const text = (e.target as HTMLElement).textContent || "";
+                  setList((p) => [...p, text]);
+                }}
+              >
+                item 1
+              </li>
+              <li>item 2</li>
+              <li>item 3</li>
+            </ul>
+          </div>
+          <div style={{ margin: "30px" }}>
+            Programming
+            <ul>
+              <li>item 4</li>
+              <li>item 5</li>
+              <li>item 6</li>
+            </ul>
+          </div>
+          <div style={{ margin: "30px" }}>
+            Personal
+            <ul>
+              <li>item 7</li>
+              <li>item 8</li>
+              <li>item 9</li>
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
