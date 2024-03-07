f34c317e3687767266f183f464a899af66045490
Corey <corey260596@gmail.com>
2024-03-07 23:24:01 +1000

    added material UI

--- src/Components/SubList.tsx
+++ src/Components/SubList.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { TextField, Button } from "@mui/material";
 // clicking a button creates a new 'SubList' which comprises of a title, then an expanding list
 // clicking done changes inputs into display 'h3'/'li' (non editable)
 
 const SubList = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", margin: "30px" }}>
-      <input placeholder="Title" />
-      <input placeholder="Task 1" />
-      <input placeholder="Task 2" />
-      <input placeholder="Task 3" />
-      <button>Done!</button>
+      <h3>
+        <TextField
+          placeholder="Title"
+          size="small"
+          inputProps={{ style: { fontSize: "1.5rem", fontWeight: "700" } }}
+        />
+      </h3>
+      <TextField placeholder="Task 1" size="small" />
+      <TextField placeholder="Task 2" size="small" />
+      <TextField placeholder="Task 3" size="small" />
+      <Button>Done!</Button>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
