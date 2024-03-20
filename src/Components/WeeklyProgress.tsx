8c10fb6a9eecf9e38083de463eb5a2f1c2bef6b9
Corey <corey260596@gmail.com>
2024-03-20 18:34:05 +1000

    adding/saving spent time

--- src/Components/WeeklyProgress.tsx
+++ src/Components/WeeklyProgress.tsx
@@ -1,18 +1,77 @@
-import { LinearProgress } from "@mui/material";
+import { FormEventHandler, useState, useEffect } from "react";
+import { LinearProgress, TextField, Button } from "@mui/material";
 // keep track of the amount of time spent on each habit
 // array of habit, time, goalTime saved locally
 // work towards a weekly goal
 // fill the tube with green when adding time
 
+type HabitTime = { habit: string; spentTime: string; goalTime: string };
+type AddingTime = { [key: string]: number };
 const WeeklyProgress = () => {
-  const habitTime = [
-    { habit: "Job Search", spentTime: 40, goalTime: 210 },
-    { habit: "Tech Study", spentTime: 20, goalTime: 210 },
-    { habit: "Project Work", spentTime: 210, goalTime: 360 },
-  ];
+  const storedHabits = localStorage.getItem("habitTimes");
+  const [habitTimes, setHabitTimes] = useState(
+    storedHabits ? JSON.parse(storedHabits) : []
+  );
+  const [habitName, setHabitName] = useState("");
+  const [goalTime, setGoalTime] = useState(210);
+
+  const [valueToAdd, setValueToAdd] = useState<AddingTime>(
+    Object.fromEntries(habitTimes.map((habit: HabitTime) => [habit.habit, 10]))
+  );
+
+  const handleAddHabit: FormEventHandler = (event) => {
+    event.preventDefault();
+    setHabitTimes((prev: HabitTime[]) => [
+      ...prev,
+      { habit: habitName, spentTime: "0", goalTime },
+    ]);
+    setHabitName("");
+    setGoalTime(210);
+    setValueToAdd((prev: AddingTime) => ({ ...prev, [habitName]: 10 }));
+  };
+
+  const handleAddTime = (habit: HabitTime, valueToAdd: AddingTime) => {
+    const updatedHabitTimes = habitTimes.map((item: HabitTime) =>
+      item.habit === habit.habit
+        ? {
+            ...item,
+            spentTime: String(+item.spentTime + valueToAdd[habit.habit]),
+          }
+        : item
+    );
+    setHabitTimes(updatedHabitTimes);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("habitTimes", JSON.stringify(habitTimes));
+  }, [habitTimes]);
+
   return (
     <div>
-      {habitTime.map((habit) => (
+      <form
+        onSubmit={handleAddHabit}
+        style={{ display: "flex", marginBottom: "30px" }}
+      >
+        <TextField
+          placeholder="Habit"
+          inputProps={{ style: { fontSize: "2rem" } }}
+          required
+          value={habitName}
+          onChange={(e) => setHabitName(e.target.value)}
+        />
+        <TextField
+          inputProps={{ style: { fontSize: "2rem" } }}
+          sx={{ width: "100px" }}
+          type="number"
+          label="Goal (min)"
+          value={goalTime}
+          onChange={(e) => setGoalTime(+e.target.value)}
+        />
+        <Button sx={{ fontSize: "2rem" }} variant="contained" type="submit">
+          Add Habit
+        </Button>
+      </form>
+      {habitTimes.map((habit: HabitTime) => (
         <div key={habit.habit}>
           <div
             style={{
@@ -28,35 +87,48 @@ const WeeklyProgress = () => {
             <LinearProgress
               color="success"
               variant="determinate"
-              value={(habit.spentTime / habit.goalTime) * 100}
+              value={(+habit.spentTime / +habit.goalTime) * 100}
               sx={{ height: "38px", minWidth: "100%" }}
             />
             <span style={{ minWidth: "100px" }}>{`${(
-              (habit.spentTime / habit.goalTime) *
+              (+habit.spentTime / +habit.goalTime) *
               100
             ).toFixed()}%`}</span>
             <div
               style={{
                 display: "flex",
-                fontSize: "1rem",
                 alignItems: "center",
                 justifyContent: "center",
+                marginLeft: "20px",
               }}
             >
               <span
                 style={{
+                  display: "flex",
                   fontSize: "3rem",
-                  verticalAlign: "center",
                 }}
               >
                 +
+                <TextField
+                  type="number"
+                  inputProps={{ style: { fontSize: "1.5rem" } }}
+                  sx={{ width: "100px" }}
+                  onChange={(e) =>
+                    setValueToAdd((prev: AddingTime) => ({
+                      ...prev,
+                      [habit.habit]: +e.target.value,
+                    }))
+                  }
+                  value={valueToAdd[habit.habit]}
+                />
               </span>
-              <input
-                type="number"
-                style={{ width: "60px", fontSize: "2rem" }}
-                defaultValue={10}
-              />
-              <button>Add Time</button>
+              <Button
+                sx={{ fontSize: "1rem", fontWeight: "800" }}
+                variant="contained"
+                onClick={() => handleAddTime(habit, valueToAdd)}
+              >
+                Add Time
+              </Button>
             </div>
           </div>
         </div>
